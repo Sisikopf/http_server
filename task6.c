c15f611390a9d6c64656d58ce59bd23e838bb384
Sisikopf <dimko12@gmail.com>
2015-11-07 15:37:16 +0300

    single-thread server with pdf, txt, html, css, jpg, png and ico file extensions support

--- task6.c
+++ task6.c
@@ -4,7 +4,7 @@
 #include <netinet/in.h>
 #include <string.h>
 
-void headers(int client, int size, int httpcode) {
+void headers(int client, int size, int httpcode, char *contentType) {
 	char buf[1024];
 	char strsize[20];
 	sprintf(strsize, "%d", size);
@@ -28,7 +28,7 @@ void headers(int client, int size, int httpcode) {
 	send(client, buf, strlen(buf), 0);
 	strcpy(buf, "simple-server\r\n");
 	send(client, buf, strlen(buf), 0);
-	sprintf(buf, "Content-Type: image/jpeg\r\n");
+	sprintf(buf, contentType);
 	send(client, buf, strlen(buf), 0);
 	strcpy(buf, "\r\n");
 	send(client, buf, strlen(buf), 0);
@@ -46,7 +46,37 @@ void parseFileName(char *line, char **filepath, size_t *len) {
 	(*filepath)[*len] = '\0';
 	printf("%s \n", *filepath);
 }
-
+int setContentType(char *filepath, char **contentType) {
+    if(strstr(filepath, ".jpg")!=NULL) {
+        *contentType = "Content-Type: image/jpeg\r\n";
+        return 1;
+    }
+    else if(strstr(filepath, ".txt")!=NULL) {
+        *contentType = "Content-Type: text/plain\r\n";
+        return 2;
+    }
+    else if(strstr(filepath, ".html")!=NULL) {
+        *contentType = "Content-Type: text/html\r\n";
+        return 3;
+    }
+    else if(strstr(filepath, ".css")!=NULL) {
+        *contentType = "Content-Type: text/css\r\n";
+        return 4;
+    }
+    else if(strstr(filepath, ".png")!=NULL) {
+        *contentType = "Content-Type: image/png\r\n";
+        return 5;
+    }
+    else if(strstr(filepath, ".pdf")!=NULL) {
+        *contentType = "Content-Type: application/pdf\r\n";
+        return 6;
+    }
+    else if(strstr(filepath, ".ico")!=NULL) {
+        *contentType = "Content-Type: image/x-icon\r\n";
+        return 7;
+    }
+    else return NULL;
+}
 int main() {
 	int ld = 0;
 	int res = 0;
@@ -64,13 +94,13 @@ int main() {
 	socklen_t size_caddr;
 	FILE *fd;
 	FILE *file;
-
+    char *contentType = (char*) malloc(255*sizeof(char));
 	ld = socket(AF_INET, SOCK_STREAM, 0);
 	if (ld == -1) {
 		printf("listener create error \n");
 	}
 	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(8080);
+	saddr.sin_port = htons(8085);
 	saddr.sin_addr.s_addr = INADDR_ANY;
 	res = bind(ld, (struct sockaddr *)&saddr, sizeof(saddr));
 	if (res == -1) {
@@ -106,21 +136,23 @@ int main() {
 			printf("%s", line);
 		}
 		printf("open %s \n", filepath);
+
 		file = fopen(filepath, "rb");
 		if (file == NULL) {
 			printf("404 File Not Found \n");
-			headers(cd, 0, 404);
+			headers(cd, 0, 404, contentType);
 		}
-		else if (file!=NULL && strstr(filepath, ".jpg")==NULL)
+		else if (file!=NULL && setContentType(filepath, &contentType)==NULL)
 		{
 			printf("500 Internal Server Error \n");
-			headers(cd, 0, 500);
+			headers(cd, 0, 500, contentType);
 		}
-		else if (file!=NULL){
+		else if (file!=NULL && setContentType(filepath, &contentType)!=NULL){
 			fseek(file, 0L, SEEK_END);
 			filesize = ftell(file);
 			fseek(file, 0L, SEEK_SET);
-			headers(cd, filesize, 200);
+			printf("%s", contentType);
+			headers(cd, filesize, 200, contentType);
 			unsigned char buf[filesize];
 			int n = fread(buf,filesize,1,file);
 			if(n==0)
