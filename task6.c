1fe09a966afa63ab5814c25caed7ed614eebb846
Sisikopf <dimko12@gmail.com>
2015-11-07 16:27:39 +0300

    Trying to make multithread server (not working)

--- task6.c
+++ task6.c
@@ -3,7 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
-
+#define THREAD_COUNT 5
 void headers(int client, int size, int httpcode, char *contentType) {
 	char buf[1024];
 	char strsize[20];
@@ -77,30 +77,90 @@ int setContentType(char *filepath, char **contentType) {
     }
     else return NULL;
 }
+void *handleClient(void *arg) {
+    int filesize = 0;
+    char *line = NULL;
+    size_t len = 0;
+    char *filepath = NULL;
+	size_t filepath_len = 0;
+	int res = 0;
+    FILE *fd;
+	FILE *file;
+	char *contentType = (char*) malloc(255*sizeof(char));
+	int cd = *((int *)arg);
+
+	int empty_str_count = 0;
+    printf("In new thread\n");
+    fd = fdopen(cd, "r");
+    if (fd == NULL) {
+        printf("error open client descriptor as file \n");
+    }
+    while ((res = getline(&line, &len, fd)) != -1) {
+        if (strstr(line, "GET")) {
+            parseFileName(line, &filepath, &filepath_len);
+        }
+        if (strcmp(line, "\r\n") == 0) {
+            empty_str_count++;
+        }
+        else {
+            empty_str_count = 0;
+        }
+        if (empty_str_count == 1) {
+            break;
+        }
+        printf("%s", line);
+    }
+    printf("open %s \n", filepath);
+
+    file = fopen(filepath, "rb");
+    if (file == NULL) {
+        printf("404 File Not Found \n");
+        headers(cd, 0, 404, contentType);
+    }
+    else if (file!=NULL && setContentType(filepath, &contentType)==NULL)
+    {
+        printf("500 Internal Server Error \n");
+        headers(cd, 0, 500, contentType);
+    }
+    else if (file!=NULL && setContentType(filepath, &contentType)!=NULL){
+        fseek(file, 0L, SEEK_END);
+        filesize = ftell(file);
+        fseek(file, 0L, SEEK_SET);
+        printf("%s", contentType);
+        headers(cd, filesize, 200, contentType);
+        unsigned char buf[filesize];
+        int n = fread(buf,filesize,1,file);
+        if(n==0)
+            printf("Read file Error");
+        res = send(cd, buf, filesize, 0);
+        if (res == -1) {
+            printf("send error \n");
+        }
+    }
+    close(cd);
+    return (void*)0;
+}
 int main() {
 	int ld = 0;
 	int res = 0;
 	int cd = 0;
-	int filesize = 0;
+    int i = 0;
 	const int backlog = 10;
 	struct sockaddr_in saddr;
 	struct sockaddr_in caddr;
-	char *line = NULL;
-	size_t len = 0;
-	char *filepath = NULL;
-	size_t filepath_len = 0;
-	int empty_str_count = 0;
+	pthread_t ntid[THREAD_COUNT];
+
+
 	socklen_t size_saddr;
 	socklen_t size_caddr;
-	FILE *fd;
-	FILE *file;
-    char *contentType = (char*) malloc(255*sizeof(char));
+
+
 	ld = socket(AF_INET, SOCK_STREAM, 0);
 	if (ld == -1) {
 		printf("listener create error \n");
 	}
 	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(8085);
+	saddr.sin_port = htons(8086);
 	saddr.sin_addr.s_addr = INADDR_ANY;
 	res = bind(ld, (struct sockaddr *)&saddr, sizeof(saddr));
 	if (res == -1) {
@@ -115,54 +175,13 @@ int main() {
 		if (cd == -1) {
 			printf("accept error \n");
 		}
-		printf("client in %d descriptor. Client addr is %d \n", cd, caddr.sin_addr.s_addr);
-		fd = fdopen(cd, "r");
-		if (fd == NULL) {
-			printf("error open client descriptor as file \n");
+        printf("client in %d descriptor. Client addr is %d \n", cd, caddr.sin_addr.s_addr);
+		int err = pthread_create(&ntid[i], NULL, &handleClient, &cd);
+		i++;
+		if (err != 0) {
+			printf("it's impossible to create a thread %s\n", strerror(err));
 		}
-		while ((res = getline(&line, &len, fd)) != -1) {
-			if (strstr(line, "GET")) {
-				parseFileName(line, &filepath, &filepath_len);
-			}
-			if (strcmp(line, "\r\n") == 0) {
-				empty_str_count++;
-			}
-			else {
-				empty_str_count = 0;
-			}
-			if (empty_str_count == 1) {
-				break;
-			}
-			printf("%s", line);
-		}
-		printf("open %s \n", filepath);
 
-		file = fopen(filepath, "rb");
-		if (file == NULL) {
-			printf("404 File Not Found \n");
-			headers(cd, 0, 404, contentType);
-		}
-		else if (file!=NULL && setContentType(filepath, &contentType)==NULL)
-		{
-			printf("500 Internal Server Error \n");
-			headers(cd, 0, 500, contentType);
-		}
-		else if (file!=NULL && setContentType(filepath, &contentType)!=NULL){
-			fseek(file, 0L, SEEK_END);
-			filesize = ftell(file);
-			fseek(file, 0L, SEEK_SET);
-			printf("%s", contentType);
-			headers(cd, filesize, 200, contentType);
-			unsigned char buf[filesize];
-			int n = fread(buf,filesize,1,file);
-			if(n==0)
-				printf("Read file Error");
-			res = send(cd, buf, filesize, 0);
-			if (res == -1) {
-				printf("send error \n");
-			}
-		}
-		close(cd);
 	}
 	return 0;
 }
